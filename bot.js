b26575db296f58cf9a1d9445830f4df7d4ed62c9
José Carlos Marquetti Jr <cinzadiscos@gmail.com>
2017-08-18 14:25:37 -0300

    Add Interface and constants
    - Add Interface to help the flow between spreadsheet and telegram.
    - Change some variables to constants

--- bot.js
+++ bot.js
@@ -8,16 +8,59 @@ var jsonfile = require('jsonfile');
 var MongoClient = require('mongodb').MongoClient;
 
 
-var SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
-var TOKEN_DIR = './.credentials/';
-var TOKEN_PATH = TOKEN_DIR + 'sheets.googleapis.com-nodejs-quickstart.json';
-var configfile = 'config.json'
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
+const TOKEN_DIR = './.credentials/';
+const TOKEN_PATH = TOKEN_DIR + 'sheets.googleapis.com-nodejs-quickstart.json';
+const configfile = 'config.json'
 
 var bot;
 var BOT_CONFIG = {};
-var BOT_CONFIG_PATH = './bot.settings.json';
+const BOT_CONFIG_PATH = './bot.settings.json';
 var BOT_PUSHING = false;
 
+//Interface types
+const INTERFACE_INFO = 1;
+const INTERFACE_TABLE = 2;
+
+var BOT_INTERFACES = {};
+
+class Interface extends Object
+{
+	constructor(name,sheet_id,type,range)
+	{
+		this.name = name;
+		this.sheet_id = sheet_id;
+		this.type = type;
+		this.range = range;
+	}
+	static add(name,chat_id,sheet_id,type,range)
+	{
+		if (!(chat_id in BOT_INTERFACES))
+		{
+			BOT_INTERFACES[chat_id]={};
+			if (!(name in BOT_INTERFACES[chat_id]))
+			{
+				BOT_INTERFACES[chat_id][name]={};
+			}
+		}
+
+		BOT_INTERFACES[chat_id][name].sheet_id=sheet_id;
+		BOT_INTERFACES[chat_id][name].type=type;
+		BOT_INTERFACES[chat_id][name].range=range;
+
+	}
+	static remove(name,chat_id)
+	{
+		if (chat_id in BOT_INTERFACES)
+		{
+
+			if (name in BOT_INTERFACES[chat_id])
+			{
+				delete BOT_INTERFACES[chat_id][name];
+			}
+		}
+	}
+}
 
 fs.readFile(BOT_CONFIG_PATH, function(err, config) {
 	if (err) {
@@ -53,7 +96,6 @@ function init_bot(token)
 	{
 		console.log("MongoDB info not found on "+BOT_CONFIG_PATH);
 	}
-
 }
 
 function bot_commands(bot)
